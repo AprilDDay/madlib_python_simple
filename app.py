9e58502b878ba47981cae7e3775a443f635d23d3
April Day <april.day@gmail.com>
2021-08-23 10:59:14 +0100

    add code for madlib game

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
-print("Roses are red.")
-print("Violets are blue.")
-print("Hope you're happy, too.")
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity name: ")
+
+print("Roses are " + color + ".")
+print(plural_noun + "are blue.")
+print("I love " + celebrity + ".")
+
